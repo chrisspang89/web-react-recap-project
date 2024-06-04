7da83295662b10afdd7dfde646960a3720c15eb6
Christian <spang.christian@yahoo.de>
2024-06-04 13:07:17 +0200

    color input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { initialColors } from "./lib/colors";
+import { useState } from "react";
 import ColorForm from "./Components/ColorForm/ColorForm";
 import Color from "./Components/Color/Color";
 import "./App.css";
 
 function App() {
+  const [colors, setColors] = useState([]);
+
+  const handleAddColor = (newColor) => {
+    setColors([...colors, newColor]);
+  };
   return (
     <>
       <h1>Theme Creator</h1>
-      <ColorForm />
+      <ColorForm onAddColor={handleAddColor} />
 
       {initialColors.map((color) => {
         return <Color key={color.id} color={color} />;
--- src/Components/ColorForm/ColorForm.jsx
+++ src/Components/ColorForm/ColorForm.jsx
@@ -2,16 +2,44 @@ import { useState } from "react";
 import { nanoid } from "nanoid";
 import ColorInput from "../ColorInput/ColorInput";
 
-export default function ColorForm() {
+export default function ColorForm({ onAddColor }) {
+  const [role, setRole] = useState("primary color");
+  const [hex, setHex] = useState("#ffffff");
+  const [contrastText, setContrastText] = useState("#000000");
+
+  const handleSubmit = (e) => {
+    e.preventDevfault();
+    onAddColor({ id: nanoid(), role, hex, contrastText });
+    setRole("primary");
+    setHex("#ffffff");
+    setContrastText("#000000");
+  };
+
   return (
-    <form className="color-form">
-      <label htmlFor="role">Role</label>
-      <input type="text" id="role" name="role" defaultValue={""} />
+    <form onSubmit={handleSubmit} className="color-form">
+      <label htmlFor="role">Role:</label>
       <br />
-      <label htmlFor="role">Hex</label>
-      <ColorInput />
+      <input
+        type="text"
+        id="role"
+        name="role"
+        value={role}
+        onChange={(e) => setRole(e.target.value)}
+      />
+      <br />
+      <label htmlFor="hex">Hex:</label>
+      <br />
+      <ColorInput id="hex" onChange={setHex} value={hex} />
+      <br />
+      <label htmlFor="contrast-text">Contrast Text:</label>
+      <br />
+      <ColorInput
+        id="contrast-text"
+        onChange={setContrastText}
+        value={contrastText}
+      />
+      <br />
+      <button>ADD COLOR</button>
     </form>
   );
 }
-
-const element = <ColorForm />;
