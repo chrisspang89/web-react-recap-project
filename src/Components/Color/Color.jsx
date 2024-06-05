f7396a16373a057cc3d2d8de3af5351a78c6a83d
Christian <spang.christian@yahoo.de>
2024-06-05 11:35:03 +0200

    add show/hide funktion to delete-button

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,7 +1,10 @@
 import "./Color.css";
 import Button from "../Button/Button";
+import { useState } from "react";
 
 export default function Color({ color, onDelete }) {
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+
   return (
     <div
       className="color-card"
@@ -13,7 +16,23 @@ export default function Color({ color, onDelete }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
-      <Button text="Delete" onButtonClick={() => onDelete(color.id)} />
+      {showDeleteConfirmation ? (
+        <>
+          <p className="color-card-headline">Really delete?</p>
+          <Button
+            text="Cancel"
+            onButtonClick={() => setShowDeleteConfirmation(false)}
+          />
+          <Button text="Delete" onButtonClick={() => onDelete(color.id)} />
+        </>
+      ) : (
+        <>
+          <Button
+            text="Delete"
+            onButtonClick={() => setShowDeleteConfirmation(true)}
+          />
+        </>
+      )}
     </div>
   );
 }
